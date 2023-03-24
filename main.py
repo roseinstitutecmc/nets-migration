7abcec4cbf4f744251dad82f5e043b44e98b6f4b
Nolan <finnless@users.noreply.github.com>
2023-03-24 07:51:29 +0000

    csv to bigquery table using schema file

--- main.py
+++ main.py
@@ -100,7 +100,36 @@ def main():
     df_time = df_end_time - df_start_time
     print('Converting took these many seconds:', df_time)
 
-    print('This is where csv should be uploaded to BigQuery')
+    print('Uploading csv to BigQuery')
+
+    # From https://cloud.google.com/bigquery/docs/batch-loading-data#python
+    # TODO(developer): Set table_id to the ID of the table to create.
+    table_id = f"rosenets.nets_import.{filename.split('.')[0]}"
+
+    # To load a schema file use the schema_from_json method.
+    # From https://cloud.google.com/bigquery/docs/schemas#python_1
+    if 'fix_ind' in filename:
+        schema_path = 'schemas/fix_ind_schema.json'
+    else:
+        schema_path = 'schemas/CA_schema.json'
+    
+    schema = bqclient.schema_from_json(schema_path)
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV, skip_leading_rows=1, schema=schema,
+    )
+
+    with open(f'{filename}.csv', "rb") as source_file:
+        job = bqclient.load_table_from_file(source_file, table_id, job_config=job_config)
+
+    job.result()  # Waits for the job to complete.
+
+    table = bqclient.get_table(table_id)  # Make an API request.
+    print(
+        "Loaded {} rows and {} columns to {}".format(
+            table.num_rows, len(table.schema), table_id
+        )
+    )
 
 
 # Start script
